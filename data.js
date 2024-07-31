7ea29f7fdd51d24b76177f4e039a6e7b111733f8
minsub12 <jejuds987@naver.com>
2024-07-31 16:16:51 +0900

    1

---
+++ data.js
@@ -0,0 +1,296 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "로비",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -1.6698760702590327,
+        "pitch": 0.16781794132345418,
+        "fov": 1.259562646316283
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.532186114484414,
+          "pitch": 0.03187221328806267,
+          "rotation": 10.995574287564278,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.7729297907617223,
+          "pitch": 0.15957138496052892,
+          "rotation": 6.283185307179586,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-_",
+      "name": "정문_주출입구",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -3.110629227122498,
+        "pitch": 0.19215250921168625,
+        "fov": 1.259562646316283
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1176194699506627,
+          "pitch": 0.08219683468450434,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "복도",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.09258566062146656,
+          "pitch": 0.25174105188445495,
+          "rotation": 6.283185307179586,
+          "target": "4--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "식당",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4--",
+      "name": "편의객실 현관",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -3.0878094154332203,
+        "pitch": 0.2276393589388892,
+        "fov": 1.259562646316283
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9698938536193022,
+          "pitch": 0.25220398472446526,
+          "rotation": 0,
+          "target": "5--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.749650833390474,
+          "pitch": 0.03205594881692875,
+          "title": "초인등",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "5--",
+      "name": "편의객실 내부",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.25537668217578435,
+        "pitch": -0.2311824976225072,
+        "fov": 1.259562646316283
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4622666137164746,
+          "pitch": 0.4305245050671971,
+          "rotation": 4.71238898038469,
+          "target": "6--"
+        },
+        {
+          "yaw": -1.3962203285844854,
+          "pitch": 0.25251240560412036,
+          "rotation": 6.283185307179586,
+          "target": "4--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.1616492921356496,
+          "pitch": -0.9409948826453309,
+          "title": "초인등",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "6--",
+      "name": "편의객실 화장실",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.724739764141532,
+          "pitch": 0.652782409162258,
+          "rotation": 7.0685834705770345,
+          "target": "5--"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
